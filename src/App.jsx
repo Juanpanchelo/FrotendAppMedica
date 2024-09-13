5dbadaac12367013fc41f5c7f64cc97fb8fc168b
Nelson Arley Villada Garcia <arley082000@gmail.com>
2024-09-12 19:34:25 -0500

    cambio routing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import test from "./components/LoginForm";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
