27437cb3b010a34085dd601fb7c1c33b8486275b
sebastian cardona <juan.cardona227@asys.edu.co>
2024-09-19 19:51:14 -0500

    ruta de registro en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm/LoginForm";
+import Register from "./components/RegisterForm/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
   );
